1234fef741045867328fb222769101f4f1cd2b8f
Bryan047 <117346025+Bryan047@users.noreply.github.com>
2024-02-29 12:45:12 +0700

    Add files via upload

---
+++ Dashboard/assets/js/progress.js
@@ -0,0 +1,57 @@
+let circularProgress = document.querySelector(".circular-progress"),
+progressValue = document.querySelector(".progress-value");
+
+let progressStartValue = 0,    
+progressEndValue = 90,    
+speed = 50;
+
+let progress = setInterval(() => {
+progressStartValue++;
+
+progressValue.textContent = `${progressStartValue}%`
+circularProgress.style.background = `conic-gradient(#7d2ae8 ${progressStartValue * 3.6}deg, #ededed 0deg)`
+
+if(progressStartValue == progressEndValue){
+    clearInterval(progress);
+}    
+}, speed);
+
+let circularProgress2 = document.querySelector(".circular-progress2"),
+progressValue2 = document.querySelector(".progress-value2");
+
+let progressStartValue2 = 0,    
+progressEndValue2 = 90,    
+speed2 = 50;
+
+let progress2 = setInterval(() => {
+progressStartValue2++;
+
+progressValue2.textContent = `${progressStartValue2}%`
+circularProgress2.style.background = `conic-gradient(#7d2ae8 ${progressStartValue2 * 3.6}deg, #ededed 0deg)`
+
+if(progressStartValue2 == progressEndValue2){
+    clearInterval(progress2);
+}    
+}, speed2);
+
+
+let circularProgress3 = document.querySelector(".circular-progress3"),
+progressValue3 = document.querySelector(".progress-value3");
+
+let progressStartValue3 = 0,    
+progressEndValue3 = 90,    
+speed3 = 50;
+
+let progress3 = setInterval(() => {
+progressStartValue3++;
+
+progressValue3.textContent = `${progressStartValue3}%`
+circularProgress3.style.background = `conic-gradient(#7d2ae8 ${progressStartValue3 * 3.6}deg, #ededed 0deg)`
+
+if(progressStartValue3 == progressEndValue3){
+    clearInterval(progress3);
+}    
+}, speed3);
+
+
+
---
+++ Dashboard/report.php
@@ -0,0 +1,87 @@
+<?php $progress = "Progress"; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Admin Dashboard | </title>
+    <!-- ======= Styles ====== -->
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <!-- =============== Navigation ================ -->
+    <div class="container">
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="logo-apple"></ion-icon>
+                        </span>
+                        <span class="title">Brand Name</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.html">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="report.php">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                       <span class="title" >Report</span>
+                    </a>
+                </li>
+
+                
+            </ul>
+        </div>
+        </div>
+        <!-- ========================= Dynamic Progress ==================== -->
+            <div class="container2">
+            <div class="circular-progress">
+                <span class="progress-value">0%</span>
+            </div>
+
+            <span class="text">HTML & CSS</span>
+            </div>
+
+            <div class="container3">
+            <div class="circular-progress2" >
+                <span class="progress-value2">0%</span>
+            </div>
+            <span class="text">HTML & CSS</span>
+            </div>
+           
+           
+
+            <div class="container4">
+            <div class="circular-progress3" >
+                <span class="progress-value3">0%</span>
+            </div>
+
+            <span class="text">HTML & CSS</span>
+            </div>
+        <!-- JavaScript -->
+        <script src="./assets/js/progress.js"></script>
+
+    <!-- =========== Scripts =========  -->
+    <script src="assets/js/main.js"></script>
+
+    <!-- ======= Charts JS ====== -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@3.7.1/dist/chart.min.js"></script>
+    <script src="assets/js/chartsJS.js"></script>
+
+    <!-- ====== ionicons ======= -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
